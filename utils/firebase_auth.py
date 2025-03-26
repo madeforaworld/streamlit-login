c5a8227afc1b4b529d6df85afcd73130a3574ba5
madeforaworld <74599428+madeforaworld@users.noreply.github.com>
2025-03-26 20:35:19 +0000

    Update firebase_auth.py

--- utils/firebase_auth.py
+++ utils/firebase_auth.py
@@ -1,1 +1,23 @@
+# utils/firebase_auth.py
 
+import pyrebase
+from utils.firebase_config import firebase_config
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+
+def sign_in_with_email(email, password):
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user
+    except Exception as e:
+        print("Login error:", e)
+        return None
+
+def sign_up_with_email(email, password):
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        return user
+    except Exception as e:
+        print("Signup error:", e)
+        return None
