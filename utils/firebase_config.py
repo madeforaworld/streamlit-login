9f490246c27d802c10b4366ac8a1f4c049c007aa
madeforaworld <74599428+madeforaworld@users.noreply.github.com>
2025-03-26 20:47:38 +0000

    Update firebase_config.py

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -1,1 +1,12 @@
+# utils/firebase_config.py
 
+firebase_config = {
+    "apiKey": "AIzaSyDb-7VXzQb86XIJH-Q5Wlgf9ccNMMD3WCg",
+    "authDomain": "mindtag-ca61c.firebaseapp.com",
+    "projectId": "mindtag-ca61c",
+    "storageBucket": "mindtag-ca61c.firebasestorage.app",
+    "messagingSenderId": "560275791939",
+    "appId": "1:560275791939:web:adea6051b949c3a3d474f6",
+    "measurementId": "G-BBL9E1DVQC",
+    "databaseURL": ""
+}
